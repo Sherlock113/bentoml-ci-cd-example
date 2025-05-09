c887762e2028a877665cad25e8f866a1d5f71c1a
Sherlock113 <sherlockxu07@gmail.com>
2025-05-09 15:44:26 +0800

    Update
    Signed-off-by: Sherlock113 <sherlockxu07@gmail.com>

--- service.py
+++ service.py
@@ -5,7 +5,7 @@ with bentoml.importing():
     from transformers import pipeline
 
 
-EXAMPLE_INPUT = "This is a test to see if the code update works. It should update the code"
+EXAMPLE_INPUT = "This is a test."
 
 
 my_image = bentoml.images.Image(python_version="3.11") \
