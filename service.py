99ab9ab105ddbfd8e7d6d2a2b420cbdc133d50b4
Sherlock Xu <65327072+Sherlock113@users.noreply.github.com>
2025-05-20 10:12:29 +0800

    Update service.py

--- service.py
+++ service.py
@@ -5,8 +5,7 @@ with bentoml.importing():
     from transformers import pipeline
 
 
-EXAMPLE_INPUT = "This is a test for code change. Here is another change to test if this works."
-
+EXAMPLE_INPUT = "Breaking News: In an astonishing turn of events, the small town of Willow Creek has been taken by storm as local resident Jerry Thompson's cat, Whiskers, performed what witnesses are calling a 'miraculous and gravity-defying leap.' Eyewitnesses report that Whiskers, an otherwise unremarkable tabby cat, jumped a record-breaking 20 feet into the air to catch a fly. The event, which took place in Thompson's backyard, is now being investigated by scientists for potential breaches in the laws of physics. Local authorities are considering a town festival to celebrate what is being hailed as 'The Leap of the Century."
 
 my_image = bentoml.images.Image(python_version="3.11") \
         .python_packages("torch", "transformers")
